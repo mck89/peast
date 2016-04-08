3965e773e27868eb7ce4fd3c36e0f2f020dff8d5
Marco <marcomarchi89@hotmail.com>
2016-04-08 19:47:13 +0000

    Implemented scanner using tokens

--- lib/Peast/Syntax/Scanner.php
+++ lib/Peast/Syntax/Scanner.php
@@ -3,25 +3,58 @@ namespace Peast\Syntax;
 
 class Scanner
 {
-    protected $source;
-    
-    protected $encoding;
-    
     protected $column = 0;
     
-    protected $line = 0;
+    protected $line = 1;
     
     protected $index = 0;
     
     protected $length;
+    
+    protected $tokens = array();
     
     function __construct($source, $encoding = null)
     {
         if ($encoding && !preg_match("/UTF-8/i", $encoding)) {
             $source = mb_convert_encoding($source, "UTF-8", $encoding);
         }
-        $this->source = $source;
-        $this->length = count($this->source);
+        
+        $tokenizer =
+            "(" .
+                "\p{Zs}|\s" . //Whitespaces
+            ")" .
+            "|" .
+            "(" .
+                    "[()[\]{}]" . //Brackets
+                "|" .
+                    "[+\-*/%|&<>^=!]?=" .  // +=, -=, *= ...
+                "|" .
+                    "/\*" . // /*
+                "|" .
+                    "\*/" . // */
+                "|" .
+                    "\|\|" . // ||
+                "|" .
+                    "&&" .  // &&
+                "|" .
+                    "\+\+" . // ++
+                "|" .
+                    "--" . // --
+                "|" .
+                    "[=!]==" . // ==, ===, !=, !==
+                "|" .
+                    "(?:<<|>>>?)=?" . // >>, >>>, >>=, >>>=, <<, <<=
+                "|" .
+                    "\.(?:\.\.)?" . // ., ...
+                "|" .
+                    "[;\"':~+\-*/%|&<>^!,?\\\\]" . // Single character symbols
+                "|" .
+                    "[^,?;\.:\"'|&=+\-*/%<>^!~\p{Zs}\s(){}[\]\\\\]+" . // Other
+            ")";
+        
+        preg_match_all("#$tokenizer#u", $source, $this->tokens, PREG_SET_ORDER);
+        
+        $this->length = count($this->tokens);
     }
     
     public function getColumn()
@@ -56,9 +89,90 @@ class Scanner
         return $this;
     }
     
+    protected function getToken()
+    {
+        if ($this->index < $this->length) {
+            $token = $this->token[$this->index];
+            $this->index++;
+            $ws = false;
+            $source = $token[0];
+            if (!isset($token[2])) {
+                $ws = true;
+                $source = preg_split("#\n|\r\n?|\x{2028}|\x{2029}#u", $source);
+            }
+            return array(
+                "source" => $source,
+                "whitespace" => $ws
+            );
+        }
+        return null;
+    }
+    
+    protected function consumeToken($token)
+    {
+        if ($token["whitespace"]) {
+            $lines = count($token["source"]) - 1;
+            $this->line += $lines;
+            $this->column += strlen($token["source"][$lines]);
+        } else {
+            $this->column += strlen($token["source"]);
+        }
+    }
+    
+    protected function unconsumeToken()
+    {
+        $this->index--;
+    }
+    
+    public function consumeWhitespacesAndComments($lineTerminator = true)
+    {
+        if (!$lineTerminator) {
+            $position = $this->getPosition();
+        }
+        $comment = $processed = 0;
+        while ($token = $this->getToken()) {
+            $processed++;
+            $source = $token["source"];
+            if ($token["whitespace"]) {
+                if (count($source) > 1) {
+                    if (!$lineTerminator) {
+                        $this->setPosition($position);
+                        return false;
+                    } elseif ($comment === 1) {
+                        $comment = 0;
+                    }
+                }
+                $this->consumeToken($token);
+            } elseif (!$comment && $source === "//") {
+                $comment = 1;
+                $this->consumeToken($token);
+            }elseif (!$comment && $source === "/*") {
+                $comment = 2;
+                $this->consumeToken($token);
+            } elseif ($comment === 2 && $source === "*/") {
+                $comment = 0;
+                $this->consumeToken($token);
+            } else {
+                $this->unconsumeToken();
+                return $processed > 1;
+            }
+        }
+        return false;
+    }
+    
     public function consume($string)
     {
         $this->consumeWhitespacesAndComments();
+        
+        $token = $this->getToken();
+        if (!$token || $token["source"] !== $string) {
+            $this->unconsumeToken();
+            return false;
+        }
+        
+        $this->consumeToken($token);
+        
+        return true;
     }
     
     public function consumeArray($sequence)
@@ -89,15 +203,10 @@ class Scanner
     public function conumeOneOf($tests)
     {
         foreach ($tests as $test) {
-            if ($this->scanner->consume()) {
+            if ($this->scanner->consume($test)) {
                 return $test;
             }
         }
         return null;
-    }
-    
-    public function consumeWhitespacesAndComments($lineTerminator = true)
-    {
-        
     }
 }
