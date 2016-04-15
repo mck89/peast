cc75579b4d04698bdbce83f13e6260281db5a1c2
Marco <marcomarchi89@hotmail.com>
2016-04-15 07:32:52 +0000

    Added TaggedTemplateExpression class

---
+++ lib/Peast/Syntax/ES6/Node/TaggedTemplateExpression.php
@@ -0,0 +1,37 @@
+<?php
+namespace Peast\Syntax\ES6\Node;
+
+class TaggedTemplateExpression extends Node implements Expression
+{
+    protected $tag;
+    
+    protected $quasi;
+    
+    public function getTag()
+    {
+        return $this->tag;
+    }
+    
+    public function setTag(Expression $tag)
+    {
+        $this->tag = $tag;
+        return $this;
+    }
+    
+    public function getQuasi()
+    {
+        return $this->quasi;
+    }
+    
+    public function setQuasi(TemplateLiteral $quasi)
+    {
+        $this->quasi = $quasi;
+        return $this;
+    }
+    
+    public function compile()
+    {
+        return $this->getTag()->compile() .
+               $this->getQuasi()->compile();
+    }
+}
