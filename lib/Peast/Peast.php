e26d045bbe364317623fcf78648eb29ca9d16a73
Marco <meccom89@live.it>
2016-07-03 14:29:53 +0200

    Removed obsolete config class, started documentation

--- lib/Peast/Peast.php
+++ lib/Peast/Peast.php
@@ -1,12 +1,53 @@
 <?php
+/**
+ * This file is part of the REBuilder package
+ *
+ * (c) Marco Marchiò <marco.mm89@gmail.com>
+ *
+ * For the full copyright and license information refer to the LICENSE file
+ * distributed with this source code
+ */
 namespace Peast;
 
+/**
+ * Main class of Peast library
+ * 
+ * @author Marco Marchiò <marco.mm89@gmail.com>
+ */
 class Peast
 {
+    //Source type constants
+    /**
+     * This source type indicates that the source is a script and import
+     * and export keywords are not parsed.
+     */
     const SOURCE_TYPE_SCRIPT = "script";
     
+    /**
+     * This source type indicates that the source is a module, this enables
+     * the parsing of import and export keywords.
+     */
     const SOURCE_TYPE_MODULE = "module";
     
+    /**
+     * Returns an instance of the ES6 parser for the given source
+     * 
+     * @param string $source  The source to parse
+     * @param array  $options Associative array for parser options. Available
+     *                        options are:
+     *                        - "sourceType": one of the source type constants
+     *                          declared in this class. This option tells the
+     *                          parser to parse the source in script or module
+     *                          mode. If this option is not provided the parser
+     *                          will work in script mode.
+     *                        - "sourceEncoding": the encoding of the given
+     *                          source. If this option is not provided the
+     *                          encoding will be detected automatically.
+     * 
+     * @return \Peast\Syntax\ES6\Parser
+     * 
+     * @static
+     */
     static public function ES6($source, $options = array())
     {
         return new Syntax\ES6\Parser($source, $options);
--- lib/Peast/Syntax/Exception.php
+++ lib/Peast/Syntax/Exception.php
@@ -1,19 +1,48 @@
 <?php
+/**
+ * This file is part of the REBuilder package
+ *
+ * (c) Marco Marchiò <marco.mm89@gmail.com>
+ *
+ * For the full copyright and license information refer to the LICENSE file
+ * distributed with this source code
+ */
 namespace Peast\Syntax;
 
 /**
+ * Syntax exception class. Syntax errors in the source are thrown using this
+ * using this exception class.
+ * 
+ * @author Marco Marchiò <marco.mm89@gmail.com>
+ * 
  * @codeCoverageIgnore
  */
 class Exception extends \Exception
 {
+    /**
+     * Error position
+     * 
+     * @var \Peast\Syntax\Position
+     */
     protected $position;
     
+    /**
+     * Class constructor
+     * 
+     * @param string                 $message  Error message
+     * @param \Peast\Syntax\Position $position Error position
+     */
     public function __construct($message, Position $position)
     {
         parent::__construct($message);
         $this->position = $position;
     }
     
+    /**
+     * Returns the error position
+     * 
+     * @return \Peast\Syntax\Position 
+     */
     public function getPosition()
     {
         return $this->position;
