7d94d1549c947f842eed220f1c4fa5d7b61c804e
Marco <meccom89@live.it>
2016-07-03 14:45:39 +0200

    Added file comment

--- lib/Peast/Syntax/ES6/Node/RegExpLiteral.php
+++ lib/Peast/Syntax/ES6/Node/RegExpLiteral.php
@@ -1,7 +1,13 @@
 <?php
+/**
+ * This file is part of the REBuilder package
+ *
+ * (c) Marco Marchiò <marco.mm89@gmail.com>
+ *
+ * For the full copyright and license information refer to the LICENSE file
+ * distributed with this source code
+ */
 namespace Peast\Syntax\ES6\Node;
-
-use Peast\Syntax\ES6\Parser;
 
 class RegExpLiteral extends Literal
 {
--- lib/Peast/Syntax/ES6/Node/VariableDeclaration.php
+++ lib/Peast/Syntax/ES6/Node/VariableDeclaration.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * This file is part of the REBuilder package
+ *
+ * (c) Marco Marchiò <marco.mm89@gmail.com>
+ *
+ * For the full copyright and license information refer to the LICENSE file
+ * distributed with this source code
+ */
 namespace Peast\Syntax\ES6\Node;
 
 class VariableDeclaration extends Node implements Declaration
