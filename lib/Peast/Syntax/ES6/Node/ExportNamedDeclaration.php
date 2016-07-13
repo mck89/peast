6d79c8b50d8fdf430d3499dbd7f0fb4337fce5dc
Marco <meccom89@live.it>
2016-07-13 20:30:49 +0200

    Documentation

--- lib/Peast/Syntax/ES6/Node/ExportNamedDeclaration.php
+++ lib/Peast/Syntax/ES6/Node/ExportNamedDeclaration.php
@@ -9,19 +9,52 @@
  */
 namespace Peast\Syntax\ES6\Node;
 
+/**
+ * A node that represents an export named declaration.
+ * For example: export {foo} from "bar"
+ * 
+ * @author Marco Marchiò <marco.mm89@gmail.com>
+ */
 class ExportNamedDeclaration extends Node implements ModuleDeclaration
 {
+    /**
+     * Exported declaration
+     * 
+     * @var Declaration 
+     */
     protected $declaration;
     
+    /**
+     * Exported specifiers
+     * 
+     * @var ExportSpecifier[] 
+     */
     protected $specifiers = array();
     
+    /**
+     * The export source
+     * 
+     * @var Literal 
+     */
     protected $source;
     
+    /**
+     * Returns the exported declaration
+     * 
+     * @return Declaration
+     */
     public function getDeclaration()
     {
         return $this->declaration;
     }
     
+    /**
+     * Sets the exported declaration
+     * 
+     * @param Declaration $declaration Exported declaration
+     * 
+     * @return $this
+     */
     public function setDeclaration($declaration)
     {
         $this->assertType($declaration, "Declaration", true);
@@ -29,11 +62,23 @@ class ExportNamedDeclaration extends Node implements ModuleDeclaration
         return $this;
     }
     
+    /**
+     * Return the exported specifiers
+     * 
+     * @return ExportSpecifier[]
+     */
     public function getSpecifiers()
     {
         return $this->specifiers;
     }
     
+    /**
+     * Sets the exported specifiers
+     * 
+     * @param ExportSpecifier[] $specifiers Exported specifiers
+     * 
+     * @return $this
+     */
     public function setSpecifiers($specifiers)
     {
         $this->assertArrayOf($specifiers, "ExportSpecifier");
@@ -41,11 +86,23 @@ class ExportNamedDeclaration extends Node implements ModuleDeclaration
         return $this;
     }
     
+    /**
+     * Returns the export source
+     * 
+     * @return Literal
+     */
     public function getSource()
     {
         return $this->source;
     }
     
+    /**
+     * Sets the export source
+     * 
+     * @param Literal $source Export source
+     * 
+     * @return $this
+     */
     public function setSource($source)
     {
         $this->assertType($source, "Literal", true);
