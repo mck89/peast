20a23b49569de344799f4487ad563bcf0987b500
Marco <marcomarchi89@hotmail.com>
2018-10-31 15:51:48 +0000

    Allow tests execution even when the library is downloaded as a composer package

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,3 +1,13 @@
 <?php
 $ds = DIRECTORY_SEPARATOR;
-include __DIR__ . $ds . ".." . $ds . "vendor" . $ds . "autoload.php";
+$paths = array(
+    ".." . $ds . "vendor",
+    ".." . $ds . ".." . $ds . ".."
+);
+foreach ($paths as $path) {
+    $abspath = __DIR__ . $ds . $path . $ds . "autoload.php";
+    if (file_exists($abspath)) {
+        require_once $abspath;
+        break;
+    }
+}
