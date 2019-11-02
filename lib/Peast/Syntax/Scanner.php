b15cd39e95781d689fa7b9c4e92e0c2c5f526652
mck89 <marcomarchi89@hotmail.com>
2019-11-02 19:16:34 +0100

    Moved some scanner properties into the base scanner class

--- lib/Peast/Syntax/Scanner.php
+++ lib/Peast/Syntax/Scanner.php
@@ -18,6 +18,8 @@ namespace Peast\Syntax;
  */
 abstract class Scanner
 {
+    use \Peast\Syntax\JSX\Scanner;
+
     /**
      * Current column
      * 
@@ -135,35 +137,49 @@ abstract class Scanner
      * 
      * @var string 
      */
-    protected $idStartRegex;
+    protected $idStartRegex = "/[\p{Lu}\p{Ll}\p{Lt}\p{Lm}\p{Lo}\p{Nl}\x{2118}\x{212E}\x{309B}\x{309C}]/u";
     
     /**
      * Regex to match identifiers parts
      * 
      * @var string 
      */
-    protected $idPartRegex;
+    protected $idPartRegex = "/[\p{Lu}\p{Ll}\p{Lt}\p{Lm}\p{Lo}\p{Nl}\x{2118}\x{212E}\x{309B}\x{309C}\p{Mn}\p{Mc}\p{Nd}\p{Pc}\x{00B7}\x{0387}\x{1369}\x{136A}\x{136B}\x{136C}\x{136D}\x{136E}\x{136F}\x{1370}\x{1371}\x{19DA}\x{200C}\x{200D}]/u";
     
     /**
      * Keywords array
      * 
      * @var array 
      */
-    protected $keywords = array();
+    protected $keywords = array(
+        "break", "do", "in", "typeof", "case", "else", "instanceof", "var",
+        "catch", "export", "new", "void", "class", "extends", "return", "while",
+        "const", "finally", "super", "with", "continue", "for", "switch",
+        "debugger", "function", "this", "default", "if", "throw",
+        "delete", "import", "try", "enum", "await"
+    );
     
     /**
      * Array of words that are keywords only in strict mode
      * 
      * @var array 
      */
-    protected $strictModeKeywords = array();
+    protected $strictModeKeywords = array(
+        "implements", "interface", "package", "private", "protected", "public",
+        "static", "let", "yield"
+    );
     
     /**
      * Punctutators array
      * 
      * @var array 
      */
-    protected $punctutators = array();
+    protected $punctutators = array(
+        ".", ";", ",", "<", ">", "<=", ">=", "==", "!=", "===", "!==", "+",
+        "-", "*", "%", "++", "--", "<<", ">>", ">>>", "&", "|", "^", "!", "~",
+        "&&", "||", "?", ":", "=", "+=", "-=", "*=", "%=", "<<=", ">>=", ">>>=",
+        "&=", "|=", "^=", "=>", "...", "/", "/="
+    );
     
     /**
      * Punctutators LSM
@@ -273,7 +289,21 @@ abstract class Scanner
     protected $xnumbers = array("0", "1", "2", "3", "4", "5", "6", "7", "8",
                                 "9", "a", "b", "c", "d", "e", "f", "A", "B",
                                 "C", "D", "E", "F");
-    
+
+    /**
+     * Octal numbers
+     *
+     * @var array
+     */
+    protected $onumbers = array("0", "1", "2", "3", "4", "5", "6", "7");
+
+    /**
+     * Binary numbers
+     *
+     * @var array
+     */
+    protected $bnumbers = array("0", "1");
+
     /**
      * Class constructor
      * 
@@ -1296,7 +1326,7 @@ abstract class Scanner
         if (!(($char >= "0" && $char <= "9") || $char === ".")) {
             return null;
         }
-        
+
         $buffer = "";
         $allowedExp = true;
         
